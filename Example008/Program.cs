a75c703b42aac80a0eea2def57423a8ac635226f
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-09 22:29:08 +0300

    add example for, text,massiv

---
+++ Example008/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int a1, int a2, int a3)
+{
+    int resulte = a1;
+if(a2 > resulte) resulte = a2;
+if(a3 > resulte) resulte = a3;
+return resulte;
+}
+int a1 = new Random().Next(0,10);
+Console.WriteLine(a1);
+int a2 = new Random().Next(0,10);
+Console.WriteLine(a2);
+int a3 = new Random().Next(0,10);
+Console.WriteLine(a3);
+Console.WriteLine(Max);
---
+++ Example3_001/Program.cs
@@ -0,0 +1,64 @@
+﻿// Вид 1
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+// Method1();
+
+// Вид 2
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
+
+// Вид 2_1
+// void Method2_1(string msg, int count)
+// {
+//    int i = 0;
+//    while(i < count)
+//    {
+//     Console.WriteLine(msg);
+//     i++;
+//    }
+// }
+// Method2_1(msg:"Текст сообщения",4); // можно ввести и в обратном порядке Method2_1(4, msg:"Текст сообщения");
+
+// Вид 3
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int Year = Method3();
+// Console.WriteLine(Year);
+
+// Вид 4
+// string Method4(int count,string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while(i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, " z");
+// Console.WriteLine(res);
+
+// Вид 4_1 FOR
+// string Method4_1(int count, string text)
+// {
+//     string result = String.Empty;
+//     for(int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4_1(10, "z ");
+// Console.WriteLine(res);
+
